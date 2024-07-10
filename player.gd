58692bd266b8804c690bfcd9084825d3095da77b
vib <vibrunazo@gmail.com>
2024-07-10 18:39:11 -0300

    10. Scripting Essentials

--- player.gd
+++ player.gd
@@ -1,13 +1,15 @@
 extends Node3D
 
+var timer: float = 0.0
+var spaces: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print('Hello world')
-	print("Don't panic")
-	print("42")
-
+	print('Player ini')
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	timer += delta
+	if Input.is_action_just_pressed("ui_accept"):
+		spaces += 1
+		print("spaces: %s, timer: %s" % [spaces, timer])
