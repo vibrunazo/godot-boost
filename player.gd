0c86b030e90920c222a84304bd5e6074caa27d55
vib <vibrunazo@gmail.com>
2024-07-11 00:42:47 -0300

    13. Instantiating the Player

--- player.gd
+++ player.gd
@@ -3,13 +3,15 @@ extends Node3D
 @export var speed: float = 2.0
 @export var gravity: float = 1.0
 
+@onready var ini_pos: Vector3 = position
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
 		position.y += delta * speed
-	if position.y >= 0:
+	if position.y >= ini_pos.y:
 		position.y -= delta * gravity
-	position.y = maxf(0, position.y)
+	position.y = maxf(ini_pos.y, position.y)
 	
 	if Input.is_action_pressed("ui_left"):
 		rotate_z(delta)
