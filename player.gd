7a8ff392f5b47d8f29e7ba8aeebb35cacefed36b
vib <vibrunazo@gmail.com>
2024-07-11 01:26:01 -0300

    14. RigidBody3D and Physics

--- player.gd
+++ player.gd
@@ -1,19 +1,15 @@
-extends Node3D
+extends RigidBody3D
 
-@export var speed: float = 2.0
-@export var gravity: float = 1.0
+@export var speed: float = 1000
 
 @onready var ini_pos: Vector3 = position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		position.y += delta * speed
-	if position.y >= ini_pos.y:
-		position.y -= delta * gravity
-	position.y = maxf(ini_pos.y, position.y)
+		apply_central_force(basis.y * delta * speed)
 	
 	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+		apply_torque(Vector3(0, 0, 100 * delta))
 	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+		apply_torque(Vector3(0, 0, -100 * delta))
