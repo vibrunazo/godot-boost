016d9aa2a8a879b7fb90509eea7cb22d9166625e
vib <vibrunazo@gmail.com>
2024-07-11 11:43:25 -0300

    16. Collision Detection

--- player.gd
+++ player.gd
@@ -4,9 +4,11 @@ extends RigidBody3D
 @export var turn_speed: float = 100
 
 @onready var ini_pos: Vector3 = position
+var is_ready: bool = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if not is_ready: return
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * speed)
 	
@@ -14,3 +16,15 @@ func _process(delta: float) -> void:
 		apply_torque(Vector3(0, 0, turn_speed * delta))
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0, 0, -turn_speed * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	print(body.name)
+	if not is_ready: return
+	if body.is_in_group("goal"):
+		print('win')
+	if body.is_in_group("hazard"):
+		print('die')
+		is_ready = false
+		await get_tree().create_timer(2).timeout
+		get_tree().reload_current_scene()
