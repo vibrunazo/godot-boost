349a31abd062550beec853b0502d6e6385215527
vib <vibrunazo@gmail.com>
2024-07-11 11:22:34 -0300

    15. The Input Map

--- player.gd
+++ player.gd
@@ -1,15 +1,16 @@
 extends RigidBody3D
 
 @export var speed: float = 1000
+@export var turn_speed: float = 100
 
 @onready var ini_pos: Vector3 = position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * speed)
 	
-	if Input.is_action_pressed("ui_left"):
-		apply_torque(Vector3(0, 0, 100 * delta))
-	if Input.is_action_pressed("ui_right"):
-		apply_torque(Vector3(0, 0, -100 * delta))
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0, 0, turn_speed * delta))
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0, 0, -turn_speed * delta))
